256a304c196972c739fc8aa9ce68988a3b5e9052
Siddhi Jaiswal <siddhijaiz23@gmail.com>
2025-09-06 21:44:19 +0530

    resolved the qr-image problem

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ let generateBtn = document.getElementById("generateBtn");
 let qrCodeImg = document.getElementById("qrCodeImg");
 let qrImageContainer = document.getElementById("qrImageContainer");
 
+let img;
+
 generateBtn.addEventListener("click", function() {
+        if(img && qrImageContainer.contains(img) ){
+                qrImageContainer.removeChild(img);
+        }
+
      QRCode.toDataURL(urlInput.value, function (err, url) {
         if (err) throw err;
-        let img = document.createElement("img");
+        img = document.createElement("img");
         img.src = url;
         img.className = "w-64 h-64";
         qrImageContainer.appendChild(img);
